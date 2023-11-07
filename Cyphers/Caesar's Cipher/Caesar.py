5fd43590e98f8ecc6ec1c8023a5ffb6383d3529b
Tortuga465 <Tem9669@yandex.ru>
2023-11-07 23:55:20 +0300

    Добавление ввода пользователем для алгоритма Цезаря. Правки

--- Cyphers/Caesar's Cipher/Caesar.py
+++ Cyphers/Caesar's Cipher/Caesar.py
@@ -1,15 +1,23 @@
+#Реализация алгоритма шифрования Цезаря
 def caesar_cipher(text, shift):
     result = ""
     for char in text:
         if char.isalpha():
-            ascii_offset = ord('a') if char.islower() else ord('A')
-            shifted_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
-            result += shifted_char
+            ascii_offset = ord('a') if char.islower() else ord('A') #Выбор точки отсчета символов: заглавных или строчных
+            shifted_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset) #Смещение символов
+            result += shifted_char #Запись результата
         else:
             result += char
     return result
 
+#Захардкоженный пример
 text = "Hello, World!"
 shift = 3
 encrypted_text = caesar_cipher(text, shift)
 print(encrypted_text)  # Output: "Khoor, Zruog!"
+
+#Пример с вводом информации пользователем
+text = input("Введите фразу, которую хотите зашифровать: \n")
+shift = int(input("Введите число, на которое хотите сместить символы: \n"))
+encrypted_text = caesar_cipher(text, shift)
+print(encrypted_text) 
