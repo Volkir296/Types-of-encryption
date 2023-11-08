160b9a909808d4090382be073beffd7bbf4788ce
Tortuga465 <Tem9669@yandex.ru>
2023-11-08 16:25:52 +0300

    Ввод данных пользователем в XOR

--- Cyphers/XOR/XOR.py
+++ Cyphers/XOR/XOR.py
@@ -14,3 +14,14 @@ print("Зашифрованное сообщение:", encrypted_message)
 
 decrypted_message = xor_encrypt_decrypt(encrypted_message, key)
 print("Расшифрованное сообщение:", decrypted_message)
+
+
+# Пример использования с помощью данных, вводимых пользователем
+message = input("Введите фразу, которую хотите зашифровать:\n")
+key = input("Введите фразу-ключ, которой фраза будет шифроваться:\n")
+
+encrypted_message = xor_encrypt_decrypt(message, key)
+print("Зашифрованное сообщение:", encrypted_message)
+
+decrypted_message = xor_encrypt_decrypt(encrypted_message, key)
+print("Расшифрованное сообщение:", decrypted_message)
