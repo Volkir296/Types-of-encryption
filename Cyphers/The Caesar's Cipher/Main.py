7f82bd7becc0a5eebb50f620428b6d89d3d0db29
MarkXXIX <Volkir296@users.noreply.github.com>
2023-11-08 17:49:23 +0300

    Update Main.py

--- Cyphers/The Caesar's Cipher/Main.py
+++ Cyphers/The Caesar's Cipher/Main.py
@@ -1,3 +1,27 @@
 from tkinter import *
 from tkinter import ttk
 
+
+#----------------------------------------------------------#
+root = Tk()
+
+root.title('The Caesar`s Cipher')
+
+# Размер экрана
+w = root.winfo_screenwidth()
+h = root.winfo_screenheight()
+
+# Середину экрана
+w = w // 2 # X
+h = h // 2 # Y
+
+# Смещение от середины
+w = w - 200 
+h = h - 200
+
+root.geometry(f'400x200+{w}+{h}')
+root.resizable(False, False)
+#----------------------------------------------------------#
+
+
+root.mainloop()
